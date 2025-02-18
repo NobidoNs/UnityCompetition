15f3d767acdd13b8f7b682f0bcdcf4ded2a64309
Lisix-Am <00lisix00@gmail.com>
2025-02-18 13:52:11 +1000

    SteeringWheel_System

---
+++ Assets/Scripts/Поворот колес.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Поворотколес : MonoBehaviour
+{
+    public GameObject LeftWheel;
+    public GameObject RightWheel;
+    public GameObject SteeringWheel;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()   
+    {   
+        Vector3 steeringRotation = SteeringWheel.transform.rotation.eulerAngles;
+        Vector3 leftWheelRotation = LeftWheel.transform.rotation.eulerAngles;
+        leftWheelRotation.y = steeringRotation.y / 2.5f;
+        LeftWheel.transform.rotation = Quaternion.Euler(leftWheelRotation);
+
+        Vector3 rightWheelRotation = RightWheel.transform.rotation.eulerAngles;
+        rightWheelRotation.y = steeringRotation.y / 2.5f;
+        RightWheel.transform.rotation = Quaternion.Euler(rightWheelRotation);
+    }
+}
