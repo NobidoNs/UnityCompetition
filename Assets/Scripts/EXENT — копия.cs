249ea95a656cec4b8c8d81037896a80df3b70945
NobidoNs <nobidon.tim@gmail.com>
2025-02-20 18:33:40 +1000

    Sounds and fix exit

--- Assets/Scripts/EXENT — копия.cs
+++ Assets/Scripts/EXENT — копия.cs
@@ -7,13 +7,16 @@ public class CarEnterExitVR : MonoBehaviour
     public GameObject car;
     public GameObject playerRig;
     public GameObject playerModel;
-    
+
+    public AudioSource audioSource; // Источник звука (можно выбрать в инспекторе)
+    public AudioClip[] radioStations;
+
     private bool isInCar = false;
     private Car_Control2 carController;
 
     public GameObject exPoint;
 
-    public GameObject Locmotion_;
+    //public GameObject Locmotion_;
 
     void Start()
     {
@@ -26,12 +29,16 @@ public class CarEnterExitVR : MonoBehaviour
         {
             playerRig.transform.position = driverSeat.position;
             playerRig.transform.rotation = driverSeat.rotation;
-            playerRig.GetComponent<LocomotionSystem>().enabled = false;
+            //playerRig.GetComponent<LocomotionSystem>().enabled = false;
             isInCar = true;
             carController.isPlayerInCar = true;
             
             if (playerModel != null)
                 playerModel.SetActive(false);
+            audioSource.Stop();
+            audioSource.clip = radioStations[0];
+            audioSource.Play();
+            Debug.Log(radioStations[0]);
         }
     }
 
@@ -40,12 +47,15 @@ public class CarEnterExitVR : MonoBehaviour
         if (isInCar)
         {
             playerRig.transform.position = exPoint.transform.position;
-            playerRig.GetComponent<CharacterController>().enabled = true;
+            //playerRig.GetComponent<CharacterController>().enabled = true;
             isInCar = false;
             carController.isPlayerInCar = false;
             
             if (playerModel != null)
                 playerModel.SetActive(true);
+            audioSource.Stop();
+            audioSource.clip = radioStations[1];
+            audioSource.Play();
         }
     }
 }
