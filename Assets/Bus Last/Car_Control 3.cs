31fe15bf3bfae00aa8b04d73a51ebf39865f07d1
Lisix-Am <00lisix00@gmail.com>
2025-02-21 09:36:33 +1000

    Move_Pref

--- Assets/Bus Last/Car_Control 3.cs
+++ Assets/Bus Last/Car_Control 3.cs
@@ -1,8 +1,15 @@
+
+
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Car_Control2 : MonoBehaviour
 {
+
+    float triggerValue, gripValue; 
     private MechanicalSystem link1;
+    private CarEnterExitVR link2;
+
     [Header("Колёса (Wheel Colliders)")]
     public WheelCollider frontLeftWheel;
     public WheelCollider frontRightWheel;
@@ -20,22 +27,31 @@ public class Car_Control2 : MonoBehaviour
     public float maxSteeringAngle = 30f;
     public float brakeForce = 3000f;
 
-    private float throttleInput;
-    private float steeringInput;
-    private bool isBraking;
+    [Header("VR Controls")]
+    public InputActionReference accelerateAction;
+    public InputActionReference brakeAction;
+    
+
+    private bool isAccelerating = false;
+    private bool isBraking = false;
 
     [Header("Состояние")]
     public bool isPlayerInCar = false;
 
     public int current = 1;
     public float[] gearRatios = { 0, -2.5f, 0f, 1.5f, 2.0f, 2.5f, 3.0f, 3.5f };
+
     private void Update()
     {
-
         if (isPlayerInCar)
         {
+            accelerateAction.action.Enable();
+            brakeAction.action.Enable();
             HandleInput();
             UpdateWheelTransforms();
+        }else{
+            accelerateAction.action.Disable();
+            brakeAction.action.Disable();
         }
     }
 
@@ -47,40 +63,44 @@ public class Car_Control2 : MonoBehaviour
             ApplyBrakes();
         }
     }
-    void HandleInput()
+
+   void HandleInput()
     {
-        throttleInput = Input.GetAxis("Vertical");
-        isBraking = Input.GetKey(KeyCode.Space);
+        triggerValue = accelerateAction.action.ReadValue<float>();
+        gripValue = brakeAction.action.ReadValue<float>();
     }
+
     void MoveVehicle()
     {
-        float motorTorque = throttleInput * maxMotorTorque;
+        float motorTorque = triggerValue * maxMotorTorque;
         motorTorque = -motorTorque;
         rearLeftWheel.motorTorque = motorTorque * gearRatios[current];
         rearRightWheel.motorTorque = motorTorque * gearRatios[current];
     }
+
     void ApplyBrakes()
     {
-        float appliedBrake = isBraking ? brakeForce : 0f;
+        float appliedBrake = gripValue * brakeForce ;
         frontLeftWheel.brakeTorque = appliedBrake;
         frontRightWheel.brakeTorque = appliedBrake;
         rearLeftWheel.brakeTorque = appliedBrake;
         rearRightWheel.brakeTorque = appliedBrake;
     }
+
     void UpdateWheelTransforms()
     {
-    UpdateWheel(frontLeftWheel, frontLeftMesh);
-    UpdateWheel(frontRightWheel, frontRightMesh);
-    UpdateWheel(rearLeftWheel, rearLeftMesh);
-    UpdateWheel(rearRightWheel, rearRightMesh);
-    }   
+        UpdateWheel(frontLeftWheel, frontLeftMesh);
+        UpdateWheel(frontRightWheel, frontRightMesh);
+        UpdateWheel(rearLeftWheel, rearLeftMesh);
+        UpdateWheel(rearRightWheel, rearRightMesh);
+    }
 
     void UpdateWheel(WheelCollider collider, Transform mesh)
     {
-    Vector3 position;
-    Quaternion rotation;
-    collider.GetWorldPose(out position, out rotation);
-    mesh.position = position;
-    mesh.rotation = rotation * Quaternion.Euler(0, 0, 90);
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+        mesh.position = position;
+        mesh.rotation = rotation * Quaternion.Euler(0, 0, 90);
     }
 }
