5bc2dd5155bc8d689133b662f19aecac18bea90f
Lisix-Am <00lisix00@gmail.com>
2025-02-20 13:35:08 +1000

    Wheels_Movement

--- Assets/Scripts/Поворот колес.cs
+++ Assets/Scripts/Поворот колес.cs
@@ -6,20 +6,15 @@ public class WheelSteering : MonoBehaviour
 
     private float totalSteeringAngle = 0f;
     private float lastSteeringRotation = 0f;
-    public float chet = 0;
 
     void Update()
     {
-        float currentRotation = SteeringWheel.transform.rotation.z;
+        float currentRotation = SteeringWheel.transform.localEulerAngles.z;
         if (currentRotation > 180) currentRotation -= 360;
         float deltaRotation = currentRotation - lastSteeringRotation;
         if (deltaRotation > 180) deltaRotation -= 360;
         if (deltaRotation < -180) deltaRotation += 360;
         totalSteeringAngle += deltaRotation;
-        chet = totalSteeringAngle;
-        if(totalSteeringAngle > 360 || totalSteeringAngle < -360){
-             SteeringWheel.transform.rotation = Quaternion.Euler(0, -90, Mathf.Clamp(currentRotation, -180, 180));
-        }
         totalSteeringAngle = Mathf.Clamp(totalSteeringAngle, -540f, 540f);
         float wheelRotation = (totalSteeringAngle / 540f) * 45f;
         LeftWheel.transform.localRotation = Quaternion.Euler(-90, wheelRotation, 0);
